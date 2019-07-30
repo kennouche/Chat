d12b95cb0ef18710737e95e9ca4b2568e0e336fa
kennouche <taoufiqkennouche@gmail.com>
2019-07-30 16:41:42 +0100

    initial commit

---
+++ lib/const.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+final themeColor = new Color(0xffff471a);
+final primaryColor = new Color(0xff203152);
+final greyColor = new Color(0xffff99ff);
+final greyColor2 = new Color(0xffff9999);
+/*
+final themeColor = new Color(0xfff5a623);
+final primaryColor = new Color(0xff203152);
+final greyColor = new Color(0xffaeaeae);
+final greyColor2 = new Color(0xffE8E8E8); */
